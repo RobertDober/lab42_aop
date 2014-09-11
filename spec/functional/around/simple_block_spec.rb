b98ae525fcc14292f80a56b89bd24dd731ed195f
Robert Dober <robert.dober@gmail.com>
2014-09-11 15:48:12 +0200

    arounds aspect result_wrapper closure tested

--- spec/functional/around/simple_block_spec.rb
+++ spec/functional/around/simple_block_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe AOP, :wip do
+describe AOP do
 
   context 'simple, block based, around aspect' do 
     before do
@@ -18,6 +18,31 @@ describe AOP, :wip do
       subject.main 41
       expect( subject.a ).to eq 42
     end
+
+    context 'aspects store results of concerns correctly' do 
+      let( :capture ){ double "capture" }
+      before do
+        capt = capture
+        class_under_test.new_method :a1 do 1 end
+        class_under_test.new_method :a2 do 2 end
+        class_under_test.new_method :a3 do 3 end
+        class_under_test.around %r{\Aa\d} do |concern|
+          capt.before
+          concern.()
+          capt.after
+        end
+      end # context 'aspects store results of concerns correct'
+      it 'calls all aspects and returns all results' do
+        3.times do
+          expect( capture ).to receive(:before).ordered
+          expect( capture ).to receive(:after).ordered
+        end
+        expect( subject.a1 ).to eq 1
+        expect( subject.a2 ).to eq 2
+        expect( subject.a3 ).to eq 3
+      end
+    end # context 'aspects store results od concerns correctly'
   end # context 'simple, block based, around aspect'
-  
+
+
 end # describe AOP
